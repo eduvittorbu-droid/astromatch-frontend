21f44b9b91aee947d85b7f57e3fe1fb433fce969
Eduardo Bueno <eduardobueno@Eduardos-MacBook-Air.local>
2026-04-17 16:07:59 -0300

    Remove tudo, deixa apenas index.html

--- services/geocode.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-export async function geocode(city, state, country) {
-  const query = `${city}, ${state}, ${country}`;
-  const url = `https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(query)}&format=json&limit=1`;
-  const res = await axios.get(url, { headers: { 'User-Agent': 'AstroMatch/1.0' } });
-  if (res.data.length === 0) throw new Error('Localidade não encontrada');
-  return { lat: parseFloat(res.data[0].lat), lng: parseFloat(res.data[0].lon) };
-}
